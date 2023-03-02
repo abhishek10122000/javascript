721386a2dcb7da465b9dd1f2cf623cd42be5afb7
abhishek10122000 <110089527+abhishek10122000@users.noreply.github.com>
2023-03-02 19:18:37 +0000

    dataTypes

---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,33 @@
+"use strict"; //treat all js code as newer version
+
+// alert('hello') // we are using node.js, not browser
+
+
+console.log(3 +
+    3)// code readability shoud be high
+
+
+console.log("anhishek")
+
+
+
+let name="abhishek"
+let age=18
+let isLoggedIn=false
+
+
+//number => 2to power 53
+//bigint 
+// string=>""
+// boolean=>true/false
+// null=>standalone value
+// undefined=>
+// symbol =>  unique
+
+// object
+
+console.log(typeof "abhishek");
+console.log(typeof age);
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
